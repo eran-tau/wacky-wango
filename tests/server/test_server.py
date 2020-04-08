b81e511d6c233e1bbddcf81de2ac7c8c1561499d
Eran Kirshenboim <kirshenboim1@mail.tau.ac.il>
2020-04-08 13:16:07 +0300

    found the problem in the test. very bad solution...

--- tests/server/test_server.py
+++ tests/server/test_server.py
@@ -7,6 +7,8 @@ from wackywango.client import upload_sample
 import time
 from multiprocessing import Process
 import json
+from wackywango.config import Config
+import os
 
 user_id = 42
 username = "Dan Gittik"
@@ -15,6 +17,8 @@ birthday = 699746400
 host = '127.0.0.1'
 port1 = 8000
 port2 = 8001
+
+config = Config()
 
 
 @pytest.fixture
@@ -63,22 +67,26 @@ def test_server_read_message(tmp_path):
 
 # This test does pass on local machine. Doesnt pass on travis
 def test_server_message_queue(tmp_path, patched_requests):
-    pass
     # Start the server
-    # time.sleep(1)
-    # print(tmp_path)
-    # test_server = Process(target=server.run_server_from_cli,
-    #                       args=(host, port2, "rabbitmq://0.0.0.0:1234"))
-    # test_server.start()
-    #
-    # # Upload the sample
-    # upload_sample(host, port2, 'tests/small_sample.mind.gz')
-    #
-    # p = tmp_path / "test2"
-    # f = open(p, "r")
-    # data = json.loads(f.read())
-    # assert data['last_parser'] == 'raw.feelings'
-    #
-    # # Make sure the server got the message
-    # test_server.terminate()
-    # test_server.join()
+
+    os.mkdir(config.data['path'])
+    os.mkdir(config.data['path']+"/raw_data/")
+
+
+    time.sleep(1)
+    print(tmp_path)
+    test_server = Process(target=server.run_server_from_cli,
+                          args=(host, port2, "rabbitmq://0.0.0.0:1234"))
+    test_server.start()
+
+    # Upload the sample
+    upload_sample(host, port2, 'tests/small_sample.mind.gz')
+
+    p = tmp_path / "test2"
+    f = open(p, "r")
+    data = json.loads(f.read())
+    assert data['last_parser'] == 'raw.feelings'
+
+    # Make sure the server got the message
+    test_server.terminate()
+    test_server.join()
